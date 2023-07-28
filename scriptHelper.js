6c58fcaedd7765544183f0ea48faba109f84cbf7
Jose Pablo Barragan <jpbarragann@gmail.com>
2023-07-28 12:21:02 -0400

    Added Validation.

--- scriptHelper.js
+++ scriptHelper.js
@@ -32,12 +32,31 @@ function formSubmission() {
     //Prevent Form Submission and Validate Submission
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event){
+        // Declare form Objects
         let pilot = document.querySelector("input[name=pilotName]");
         let copilot = document.querySelector("input[name=copilotName]");
         let fuelLevel = document.querySelector("input[name=fuelLevel]");
         let cargoLevel = document.querySelector("input[name=cargoMass]");
+        // Declare Awaiting Information Objects
+        let faultyItems = document.getElementById("faultyItems");
+        let pilotStatus = document.getElementById("pilotStatus");
+        let copilotStatus = document.getElementById("copilotStatus");
+        let lauchStatus = document.getElementById("launchStatus");
+        // Check if all fields are completed
         if (validateInput(pilot.value) === "Empty" || validateInput(copilot.value) === "Empty" || validateInput(fuelLevel.value) === "Empty" || validateInput(cargoLevel.value) === "Empty"){
             alert("All fields are required!");
+            event.preventDefault();
+        }
+        // Check if fields include the correct format & update "Awaiting Information"
+        if (validateInput(pilot.value) !== "Not a Number" || validateInput(copilot.value) !== "Not a Number" || validateInput(fuelLevel.value) === "Not a Number" || validateInput(cargoLevel.value) === "Not a Number"){
+            alert("Input must be in the correct format!");
+            event.preventDefault();
+        } else {
+            faultyItems.style.visibility = "visible";
+            launchStatus.innerHTML = "Shuttle is ready for launch";
+            lauchStatus.style.color = "#419F6A"; 
+            pilotStatus.innerHTML = `Pilot ${pilot.value} is ready for launch`;
+            copilotStatus.innerHTML = `Co-pilot ${copilot.value} is ready for launch`;
             event.preventDefault();
         }
    });
